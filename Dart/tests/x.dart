2455320b498ffe41d8b61c0bd7442186a08cd5e2
Martijn Dwars <ikben@martijndwars.nl>
2015-12-11 10:29:43 +0100

    Many working directory changes

---
+++ Dart/tests/x.dart
@@ -0,0 +1,24 @@
+import 'y.dart';
+
+main() {
+  foo();
+  bar();
+  baz();
+  
+  final String $tring = "";
+  final f = null;
+  const String sc = "";
+  const ac = 1;
+  var v = 1 + 2;
+  int i = 42 * 0;
+
+  final String
+    $tring1 = "But could you play",
+    $tring3 = "right to the finish",
+    $tring4 = "a nocturne on a drainpipe flute";
+
+    var x, y = 1 + 2, z = 3 * 4, xc = ac+1;
+    double d, d1 = 0.5, d2;
+}
+
+
---
+++ Dart/tests/y.dart
@@ -0,0 +1,9 @@
+int foo() {
+  
+}
+
+int bar() {
+	
+}
+
+boolean baz() {}
