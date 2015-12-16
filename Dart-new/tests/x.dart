b05a0f4c9991c9183ea340ade24869cb6b4d03ce
Martijn Dwars <ikben@martijndwars.nl>
2015-12-16 20:51:26 +0100

    Working directory changes

--- Dart-new/tests/x.dart
+++ Dart-new/tests/x.dart
@@ -1,20 +1,5 @@
-class A {
-  method() {
-    {
-      var i;
-    }
-
-    for (var i = 0; i < 10; i++) {}
-
-    void f(i) {}
-  }
-}
-
 main() {
-  l() {
-    var local;
-  };
-  {
-    var local = 1;
-  }
+  try {
+    const {"key": [1, 2]};
+  } catch(e) {}
 }
